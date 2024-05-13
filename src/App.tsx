12a8c3a97707d28be2a2f30578744f8d4b36a63b
Oleh <70809643+oleg94simf@users.noreply.github.com>
2024-05-13 19:48:36 +0300

    ex

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 
 const App = () => {
+  const poe = "sd";
   return (
     <>
       <RouterProvider router={router} />
