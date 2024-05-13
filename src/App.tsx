ce2a64b25ec3fb607b07a0c36c65af3d09fae939
Oleh <70809643+oleg94simf@users.noreply.github.com>
2024-05-13 19:50:40 +0300

    add git

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 
 const App = () => {
-  const poe = "sd";
   return (
     <>
       <RouterProvider router={router} />
